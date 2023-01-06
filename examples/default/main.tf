7988dd609c639a74d1c47b4be72b6b8834126b3a
Emma Dixneuf <emmadixneuf@hotmail.fr>
2023-01-06 16:20:51 +0100

    fix(data): remove subnet data

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,7 +1,9 @@
 module "bastion" {
-  source           = "../.."
-  project_id       = "padok-playground"
-  name             = "bastion"
-  subnet_self_link = "projects/padok-playground/regions/europe-west1/subnetworks/production"
-  members          = ["user:annefloref@padok.fr"]
+  source            = "../.."
+  project_id        = "padok-playground"
+  region            = "europe-west-3"
+  name              = "bastion"
+  network_self_link = "projects/padok-playground/regions/europe-west1/networks/production"
+  subnet_self_link  = "projects/padok-playground/regions/europe-west1/subnetworks/production" 
+  members           = ["user:annefloref@padok.fr"]
 }
